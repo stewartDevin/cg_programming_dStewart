39352ba75db3c486ebad9eec1d5d2f1bb31c410b
unknown <r202-stu@AI241LAB0202P13.aipdcls.edu>
2015-09-02 22:01:35 -0700

    added fade in shader

--- cg_programming_dStewart/Assets/Code/APP/APP.Scene.cpp
+++ cg_programming_dStewart/Assets/Code/APP/APP.Scene.cpp
@@ -130,10 +130,14 @@ Mesh* skyBox = NULL;
 Mesh* landscapeMesh = NULL;
 
 void LoadBunny() {
+	/////////////////////////////
+	// load toon material
 	toonBunnyMaterial = Material::CreateMaterial("./Assets/Images/soft-brown-fur-texture.png");
 	toonBunnyMaterial->shaderID = Load::LoadShaders("./Assets/Shaders/Toon.vertexshader", "./Assets/Shaders/Toon.fragmentshader");
 	toonBunnyMaterial->diffuseTiling = vec2(10.0f);
 
+	/////////////////////////////
+	// load blinn material
 	blinnBunnyMaterial = Material::CreateMaterial("./Assets/Images/soft-brown-fur-texture.png");
 	blinnBunnyMaterial->shaderID = Load::LoadShaders("./Assets/Shaders/BlinnFresnel.vertexshader", "./Assets/Shaders/BlinnFresnel.fragmentshader");
 	blinnBunnyMaterial->diffuseTiling = vec2(10.0f);
@@ -141,12 +145,15 @@ void LoadBunny() {
 	//blinnBunnyMaterial->specularImageFilePath[0] = "./Assets/Images/specMapTest.jpg";
 	Load::_LoadTexture(&blinnBunnyMaterial->specularImageID[0], blinnBunnyMaterial->specularImageFilePath[0]);
 
-	/*fadeInBunnyMaterial = Material::CreateMaterial("./Assets/Images/soft-brown-fur-texture.png");
+	/////////////////////////////
+	// load Fade-In material
+	fadeInBunnyMaterial = Material::CreateMaterial("./Assets/Images/soft-brown-fur-texture.png");
 	fadeInBunnyMaterial->shaderID = Load::LoadShaders("./Assets/Shaders/FadeInShaders/FadeIn.vertexshader", "./Assets/Shaders/FadeInShaders/FadeIn.fragmentshader");
-	fadeInBunnyMaterial->diffuseTiling = vec2(10.0f);*/
+	fadeInBunnyMaterial->diffuseTiling = vec2(10.0f);
+	fadeInBunnyMaterial->specularImageFilePath[0] = "./Assets/Images/fadeIn_boxes_TopToBottom.png";
+	Load::_LoadTexture(&fadeInBunnyMaterial->specularImageID[0], fadeInBunnyMaterial->specularImageFilePath[0]);
 
-	//bunnyMesh1 = Mesh::CreateMeshObject("./Assets/Models/bunnyAveraged.obj", *toonBunnyMaterial, Transform(vec3(0.0f, 0.0f, 10.0f)));
-	bunnyMesh1 = Mesh::CreateMeshObject("./Assets/Models/bunnyAveraged.obj", *blinnBunnyMaterial, Transform(vec3(0.0f, 0.0f, 10.0f)));
+	bunnyMesh1 = Mesh::CreateMeshObject("./Assets/Models/bunnyAveraged.obj", *fadeInBunnyMaterial, Transform(vec3(0.0f, 0.0f, 10.0f)));
 
 	//bunnyMesh2 = Mesh::CreateMeshObject("./Assets/Models/bunnyAveraged.obj", *toonBunnyMaterial, Transform(vec3(2.0f, 0.0f, 12.0f)));
 }
@@ -155,6 +162,28 @@ void RunBunny() {
 	if (bunnyMesh1 != NULL) bunnyMesh1->transform.Rotate(0.4f * DataCore::deltaTime, vec3(0.0f, 1.0f, 0.0f), false);
 	if (bunnyMesh2 != NULL) bunnyMesh2->transform.Rotate(0.4f * DataCore::deltaTime, vec3(0.0f, 1.0f, 0.0f), false);
 
+	static GLfloat fadeInTimer = 0.0f;
+	static float direction = 1.0f;
+	if(fadeInBunnyMaterial != NULL) {
+		
+		glUseProgram(fadeInBunnyMaterial->shaderID);
+		// get the timer variable on the video card
+		GLint timeLoc = glGetUniformLocationARB(fadeInBunnyMaterial->shaderID, "fadeInTimer");
+		// send the timer to the vertex Shader
+		glUniform1fARB(timeLoc, fadeInTimer);
+
+		fadeInTimer += 0.3f * direction * DataCore::deltaTime;
+
+		if(fadeInTimer >= 1.0f) {
+			fadeInTimer = 1.0f;
+			direction = -1.0f;
+		} 
+		else if(fadeInTimer <= 0.0f) {
+			fadeInTimer = 0.0f;
+			direction = 1.0f;
+		} 
+	
+	}
 }
 
 void RunLight() {
@@ -189,6 +218,12 @@ void RunLight() {
 	if (blinnBunnyMaterial != NULL) {
 		glUseProgram(blinnBunnyMaterial->shaderID);
 		GLint lightLocation = glGetUniformLocationARB(blinnBunnyMaterial->shaderID, "lightDirection");
+		glUniform3f(lightLocation, lightDirection.x, lightDirection.y, lightDirection.z);
+	}
+
+	if (fadeInBunnyMaterial != NULL) {
+		glUseProgram(fadeInBunnyMaterial->shaderID);
+		GLint lightLocation = glGetUniformLocationARB(fadeInBunnyMaterial->shaderID, "lightDirection");
 		glUniform3f(lightLocation, lightDirection.x, lightDirection.y, lightDirection.z);
 	}
 
